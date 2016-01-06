536955f8cd553f1ab6ef97dfb7415959209431de
Thell Fowler <thell@tbfowler.name>
2016-01-05 22:32:00 -0600

    tests: use SEXP return type in gmp-type.cpp funcs
    Signed-off-by: Thell Fowler <thell@tbfowler.name>

--- tests/testthat/cpp/gmp-type.cpp
+++ tests/testthat/cpp/gmp-type.cpp
@@ -6,25 +6,25 @@
 using namespace Rcpp;
 
 // [[Rcpp::export("as.bigz_uint32")]]
-RawVector as_bigz_uint32(uint32_t x) {
+SEXP as_bigz_uint32(uint32_t x) {
   mpz_class xmp(x);
   return wrap(xmp);
 }
 
 // [[Rcpp::export("as.bigz_str")]]
-RawVector as_bigz_str(std::string x) {
+SEXP as_bigz_str(std::string x) {
   mpz_class xmp(x);
   return wrap(xmp);
 }
 
 // [[Rcpp::export("as.bigq_uint32")]]
-RawVector as_bigq_uint32(uint32_t n, uint32_t d) {
+SEXP as_bigq_uint32(uint32_t n, uint32_t d) {
   mpq_class xmp(n, d);
   return wrap(xmp);
 }
 
 // [[Rcpp::export("as.bigq_str")]]
-RawVector as_bigq_str(std::string n, std::string d) {
+SEXP as_bigq_str(std::string n, std::string d) {
   mpq_class xmp(n);
   xmp /= mpq_class(d);
   return wrap(xmp);
