a380814a25adc7c05a7be88cf1e6ad93e318c7d2
Thell Fowler <thell@tbfowler.name>
2016-01-07 22:14:49 -0600

    mpreal: make wrapper a pass-thru to mpfr_t
    - Refactor wrap() of mpfr::mpreal to a cast mpfr_t from the mpfr_ptr.
    
    Signed-off-by: Thell Fowler <thell@tbfowler.name>

--- inst/include/RcppMP_mpreal.hpp
+++ inst/include/RcppMP_mpreal.hpp
@@ -16,42 +16,14 @@
 
 namespace Rcpp {
 
-template <> SEXP wrap(const mpfr::mpreal &in) {
-  constexpr auto rlimb_factor(GMP_NUMB_BITS / 32); // 1 or 2
-  constexpr auto mask(0x00000000FFFFFFFFL);
-
-  mpfr_srcptr src(in.mpfr_srcptr());
-  uint32_t limbs(1 + ((src->_mpfr_prec - 1) / mp_bits_per_limb));
-  IntegerVector out_data(rlimb_factor * limbs);
-  IntegerVector out_exp(2);
-
-#if GMP_NUMB_BITS == 32
-  out_exp[0] = src->_mpfr_exp;
-  for (int i = 0; i < limbs; i++) {
-    out_data[i] = (uint32_t)src->_mpfr_d[i];
-  }
-#elif GMP_NUMB_BITS == 64
-  out_exp[0] = src->_mpfr_exp & mask;
-  out_exp[1] = src->_mpfr_exp >> 32;
-  for (int i = 0, j = 0; j < limbs; i += 2, j++) {
-    out_data[i] = (uint32_t)(src->_mpfr_d[j] & mask);
-    out_data[i + 1] = (uint32_t)(src->_mpfr_d[j] >> 32);
-  }
-#endif
-
-  S4 out("mpfr1");
-  out.slot("prec") = (int32_t)src->_mpfr_prec;
-  out.slot("exp") = out_exp;
-  out.slot("sign") = (int32_t)src->_mpfr_sign;
-  out.slot("d") = out_data;
-
-  return (out);
+template <> SEXP wrap(const mpfr::mpreal &x) {
+  return wrap((mpfr_t){*x.mpfr_ptr()});
 }
 
 template <> SEXP wrap(const std::vector<mpfr::mpreal> &x) {
   List xS4(x.size());
   for (size_t i = 0; i < x.size(); ++i) {
-    xS4[i] = Rcpp::wrap(x[i]);
+    xS4[i] = wrap(x[i]);
   }
 
   S4 out("mpfr");
